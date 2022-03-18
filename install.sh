0ea2f00073b68a0b51212bd032590ea27af5459c
ashkumbatsiuk <ashkumbatsiuk@kyriba.com>
2022-03-18 16:29:36 +0300

    tmp

---
+++ install.sh
@@ -0,0 +1,12 @@
+
+# created based on https://vc.ru/dev/66942-sozdaem-svoy-vpn-server-poshagovaya-instrukciya
+
+apt-get update
+apt-get upgrade
+apt-get install strongswan
+apt-get install libstrongswan-standard-plugins
+
+# certificates
+apt-get install strongswan-pki
+
+
